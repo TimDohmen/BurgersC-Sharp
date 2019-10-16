c56ef9c881cb83662f088f63088a3b8f54db5350
Tim Dohmen <dohmentim@yahoo.com>
2019-10-15 22:33:17 -0600

    services setup need dapper

---
+++ Repositories/BurgerRepository.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using burgercats.Models;
+
+namespace burgercats.Repositories
+{
+  public class BurgerRepository
+  {
+
+    internal IEnumerable<Burger> Get()
+    {
+      throw new NotImplementedException();
+    }
+
+    internal Burger Get(string id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal void Create(Burger newBurger)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal void Edit(Burger editBurgerData)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal void Remove(string id)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
